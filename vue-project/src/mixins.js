79121027eaa1503c196e70fac687124b43db1636
gaebaljoah <ljm5436124@naver.com>
2023-11-16 14:52:50 +0900

    axios 패키지 추가

---
+++ vue-project/src/mixins.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export default {
+    method:{
+        async $api(url, method, data) {
+            return (await axios({
+                method: method,
+                url,
+                data            
+            }).catch(e => {
+                console.log(e);
+            })).data
+        }
+    }
+}
